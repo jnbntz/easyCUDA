4f0bf156431116ddc5033e622d8c2848f4a3a310
Jonathan Bentz <jnbntz@users.noreply.github.com>
2024-09-05 15:10:23 -0500

    Create add_cpu.cpp

---
+++ add_cpu.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+
+// function to add the elements of two arrays
+void add(int n, float *sum, float *x, float *y)
+{
+ for (int i = 0; i < n; i++)
+     sum[i] = x[i] + y[i];
+}
+
+int main(void)
+{
+ int N = 1<<20; // 1M elements
+
+ float *x = new float[N];
+ float *y = new float[N];
+ float *sum = new float[N];
+
+ // initialize x and y arrays on the host
+ for (int i = 0; i < N; i++) {
+   x[i] = 1.0f;
+   y[i] = 2.0f;
+   sum[i] = 0.0f;
+ }
+
+ // Run kernel on 1M elements on the CPU
+ add(N, sum, x, y);
+
+ // Check for errors (all values should be 3.0f)
+ float maxError = 0.0f;
+ for (int i = 0; i < N; i++)
+   maxError = fmax(maxError, fabs(sum[i]-3.0f));
+ std::cout << "Max error: " << maxError << std::endl;
+
+ // Free memory
+ delete [] x;
+ delete [] y;
+ delete [] sum;
+
+ return 0;
+}
+
